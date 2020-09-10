5ae95d7398d3ec02dbdb3546ff46ca17a952fa5e
tudou0002 <1244491123@qq.com>
2020-09-10 09:11:15 -0400

    Added score attribute to the entity type

--- extractors/entity.py
+++ extractors/entity.py
@@ -1,15 +1,16 @@
 
 class Entity:
-    def __init__(self, text: str, category:str, begin_offset: int):
+    def __init__(self, text: str, begin_offset: int, score=-1.0):
         """Construct an entity object.
 
         text: the plain text of this entity.
-        category: The type of entity
+        score: The certainty of this entity being a name. 
+            Default is -1.0 meaning the certainty is not calculated by word embedding.
         begin_offset: The index of the entity within the parent document.
 
         """
         self.text = text
-        self.category = category
+        self.score = score
         self.begin_offset = begin_offset
         self.end_offset = begin_offset + len(text)
 
@@ -25,7 +26,7 @@ class Entity:
         same attributes and from the same parent document. 
 
         """
-        return self.text == other.text and self.category == other.category and self.begin_offset == other.begin_offset
+        return self.text == other.text and self.begin_offset == other.begin_offset
 
     def __hash__(self):
-        return hash((self.text, self.category, self.begin_offset))
+        return hash((self.text, self.begin_offset))
