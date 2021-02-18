d7b50cd873f1481ab2dac56e1a5fb1f5f36aecca
nair-p <96.pratheek@gmail.com>
2021-02-18 14:48:36 -0500

    adding ht-bert predictions

---
+++ baselines/bert-model/merge_datasets.py
@@ -0,0 +1,309 @@
+# This script merges the formatted custom datasets with the ConLL 2003 dataset for fine-tuning bert model for NER
+
+import argparse
+from datasets import load_dataset
+import ast
+import pandas as pd
+
+from tqdm import tqdm
+
+from tokenizers.implementations import ByteLevelBPETokenizer
+from tokenizers.processors import BertProcessing
+
+from transformers import RobertaTokenizerFast
+from transformers import RobertaConfig
+from transformers import RobertaForMaskedLM, RobertaForTokenClassification
+
+import os
+import re
+import logging
+import unicodedata
+from html.parser import HTMLParser
+import emoji
+import unidecode
+
+
+
+tr_size = 1000
+test_size = 100
+
+# compile regexes
+username_regex = re.compile(r'(^|[^@\w])@(\w{1,15})\b')
+url_regex = re.compile(r'((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))')
+control_char_regex = re.compile(r'[\r\n\t]+')
+# translate table for punctuation
+transl_table = dict([(ord(x), ord(y)) for x, y in zip(u"‘’´“”–-",  u"'''\"\"--")])
+# HTML parser
+html_parser = HTMLParser()
+
+def string_to_list(input):
+	# function to convert a string of a list to a list
+	if type(input) == list or input is None:
+		return input
+	new_list = []
+	for item in input.split(','):
+		if item == 'set()':
+			new_list.append('')
+			continue
+		elif item[0] == '[':
+			if item[1] == "'":
+				it = item[2:-1]
+			else:
+				it = item[1:]
+		elif item[-1] == ']':
+			if item[0] == ' ':
+				it = item[1:-1]
+			else:
+				it = item[:-1]
+			if it[0] == "'":
+				it = it[1:-1]
+		elif item[0] == ' ':
+			it = item[1:]
+			if it[0] == "'" or it[0] == '"':
+					it = it[1:-1]
+			else:
+					it = it[:]
+		else:
+			it = item[1:-1]
+			#print(it)
+		new_list.append(it)
+	return new_list
+
+def apply_mapping(x):
+	tag_correction_map = {'0':'0','I-PER':'I-PER','B-PER':'B-PER','1':'B-PER', '2':'I-PER', '3':'B-ORG', '4':'I-ORG', \
+	'5':'B-LOC','6':"I-LOC",'7':"B-MISC",'8':"I-MISC"}
+
+	return tag_correction_map[x]
+
+def merge(data_file_path, conll):
+	# merges the custom HT dataset with the conll for downstream NER
+	#name_splits = data_file_path.split('_')
+	#file_prefix = name_splits[0] + "_" + name_splits[1] + "_" + name_splits[2] + "_"
+	train_file_name = data_file_path + "_train.csv"
+	test_file_name = data_file_path + "_test.csv"
+	custom_train = pd.read_csv(train_file_name)
+	custom_test = pd.read_csv(test_file_name)
+
+	for id, item in enumerate(conll['train']['tokens'][:tr_size]):
+		custom_train = custom_train.append({'tokens':item, 'ner_tags':conll['train']['ner_tags'][id]}, ignore_index=True)
+
+	for id, item in enumerate(conll['test']['tokens'][:test_size]):
+		custom_test = custom_test.append({'tokens':item, 'ner_tags':conll['test']['ner_tags'][id]}, ignore_index=True)
+
+	print(custom_train.shape)
+	print(custom_test.shape)
+	return custom_train, custom_test
+
+
+def load_conll():
+	# returns the conll dataset
+	conll = load_dataset('conll2003')
+	new_conll = {'train':{'tokens':[None]*tr_size, 'ner_tags':[None]*tr_size}, 'test':{'tokens':[None]*test_size, 'ner_tags':[None]*test_size}}
+	# removing class labels other than B-PER and I-PER
+	for id, item in tqdm(enumerate(conll['train']['ner_tags'][:tr_size])):
+		new_list = []
+		new_conll['train']['tokens'][id] = conll['train']['tokens'][id]
+		new_conll['train']['ner_tags'][id] = item
+
+		for i, x in enumerate(item):
+			if x == 1:
+				new_list.append('PER')
+			elif x == 2:
+				new_list.append('PER')
+			else:
+				new_list.append('0')
+
+		#new_conll['train']['ner_tags'][id] = new_list
+
+	for id, item in tqdm(enumerate(conll['test']['ner_tags'][:test_size])):
+		new_list = []
+		new_conll['test']['tokens'][id] = conll['test']['tokens'][id]
+		new_conll['test']['tokens'][id] = item
+
+		for i, x in enumerate(item):
+			if x == 1:
+				new_list.append('PER')
+			elif x == 2:
+				new_list.append('PER')
+			else:
+				new_list.append('0')
+		#new_conll['test']['ner_tags'][id] = new_list
+
+	# print(conll['train']['tokens'][5])
+	# print(conll['train']['ner_tags'][5])
+	return new_conll
+
+
+def preprocess_bert(text, do_lower_case=False):
+	"""Preprocesses input for NER"""
+	# standardize
+	text = standardize_text(text)
+	text = asciify_emojis(text)
+	text = standardize_punctuation(text)
+	if do_lower_case:
+		text = text.lower()
+	text = remove_unicode_symbols(text)
+	text = remove_accented_characters(text)
+	return text
+
+def remove_accented_characters(text):
+	text = unidecode.unidecode(text)
+	return text
+
+def remove_unicode_symbols(text):
+	text = ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'So')
+	return text
+
+def asciify_emojis(text):
+	"""
+	Converts emojis into text aliases. E.g. 👍 becomes :thumbs_up:
+	For a full list of text aliases see: https://www.webfx.com/tools/emoji-cheat-sheet/
+	"""
+	text = emoji.demojize(text)
+	return text
+
+def standardize_text(text):
+	"""
+	1) Escape HTML
+	2) Replaces some non-standard punctuation with standard versions. 
+	3) Replace \r, \n and \t with white spaces
+	4) Removes all other control characters and the NULL byte
+	5) Removes duplicate white spaces
+	"""
+	# escape HTML symbols
+	text = html_parser.unescape(text)
+	# standardize punctuation
+	text = text.translate(transl_table)
+	text = text.replace('…', '...')
+	# replace \t, \n and \r characters by a whitespace
+	text = re.sub(control_char_regex, ' ', text)
+	# remove all remaining control characters
+	text = ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'C')
+	# replace multiple spaces with single space
+	text = ' '.join(text.split())
+	return text.strip()
+
+def standardize_punctuation(text):
+	return ''.join([unidecode.unidecode(t) if unicodedata.category(t)[0] == 'P' else t for t in text])
+
+def get_tags_in_format(list_of_text, list_of_names, filename):
+	all_words = []
+	all_tags = []
+	all_ids = []
+	#print(list_of_names[29])
+	for id, text in enumerate(list_of_text):
+		split_by_comma = text.split(',') # we have to do this as some names appear just before a comma without /
+										# spaces and we will miss those
+		words_in_text = []
+		tags = []
+		all_ids.append(str(id))
+		for txt in split_by_comma: # splitting again by spaces. So essentially splitting by both comma & space
+			words_in_text.extend(txt.split())
+		#print(type(list_of_names[id]))
+		for word in words_in_text:
+			#print(word, list_of_names[id], id)
+			w = word.lower()
+			if w in list_of_names[id] or w == list_of_names[id][0][:-1] and len(w) > 1:
+				if w in words_in_text[:3]: # tag is B-PER
+					tag = "B-PER"
+				else:
+					tag = "I-PER"
+				print(word, list_of_names[id], id)
+				# #tag = list_of_tags[id]
+				# if list_of_tags[id] == 'I-PER':
+				#     tag = 1
+				# else:
+				#     tag = 2
+			else:
+				tag = 0
+			tags.append(tag)
+		
+		all_words.append(words_in_text)
+		all_tags.append(tags)
+		
+	new_data = {'id':all_ids, 'tokens':all_words, 'ner_tags':all_tags}
+	#test_data = {'id':all_ids[tr_size+1:], 'tokens':all_words[tr_size+1:], 'ner_tags':all_tags[tr_size+1]}
+	df = pd.DataFrame(new_data)
+	#test_df = pd.DataFrame(test_data)
+	df.to_csv(filename)
+	#test_df.to_csv(filename+"_test.csv")
+	#return train_df, test_df
+	return df
+
+def load_custom_data(train_data_file, test_data_file, train_desc_column, test_desc_column, train_tag_column, test_tag_column):
+
+	train_data = pd.read_csv(train_data_file, sep='\t')
+	test_data = pd.read_csv(test_data_file, sep='\t')
+	print(train_data[train_tag_column].iloc[90])
+	train_data['processed_text'] = train_data[train_desc_column].apply(lambda x: preprocess_bert(x))
+	train_data[train_tag_column] = train_data[train_tag_column].apply(lambda x: string_to_list(x))
+
+	test_data['processed_text'] = test_data[test_desc_column].apply(lambda x: preprocess_bert(x))
+	test_data[test_tag_column] = test_data[test_tag_column].apply(lambda x: string_to_list(x))
+
+	new_data_train = get_tags_in_format(train_data['processed_text'], train_data[train_tag_column], "/home/pnair6/McGill/Research/HT/NER/bert_ner_fine_tune/data_feb18/ht_formatted_data_train.csv")
+	new_data_test = get_tags_in_format(test_data['processed_text'], test_data[test_tag_column], "/home/pnair6/McGill/Research/HT/NER/bert_ner_fine_tune/data_feb18/ht_formatted_data_test.csv")
+	return new_data_train, new_data_test
+	# data[[desc_column, tag_column]].to_csv(data_file.split('/')[-1])
+	# return data[[desc_column, tag_column]]
+
+def csv_to_txt(train, test):
+
+	new_train = open("/home/pnair6/McGill/Research/HT/NER/bert_ner_fine_tune/data_feb18/train.txt", "w")
+	new_test = open("/home/pnair6/McGill/Research/HT/NER/bert_ner_fine_tune/data_feb18/test.txt", "w")
+
+	id_to_tag_map = {'0':'0', '1':'B-PER', '2':'I-PER'}
+
+	train['tokens'] = train['tokens'].apply(lambda x: string_to_list(x))
+	test['tokens'] = test['tokens'].apply(lambda x: string_to_list(x))
+
+	print(train.iloc[284]['ner_tags'])
+
+	train['ner_tags'] = train['ner_tags'].apply(lambda x: string_to_list(x))
+	test['ner_tags'] = test['ner_tags'].apply(lambda x: string_to_list(x))
+
+	print(train.shape)
+	print(test.shape)
+
+	for id, row in tqdm(train.iterrows()):
+		for i, tok in enumerate(row['tokens']):
+			try:
+				new_train.write(tok + " NULL " + apply_mapping(row['ner_tags'][i]) + "\n")
+			except:
+				new_train.write(tok + " NULL " + apply_mapping(row['ner_tags'][i][:-1]) + "\n")
+		new_train.write("\n")
+
+	for id, row in tqdm(test.iterrows()):
+		for i, tok in enumerate(row['tokens']):
+			new_test.write(tok + " NULL " + apply_mapping(row['ner_tags'][i]) + "\n")
+		new_test.write("\n")
+
+	new_train.close()
+	new_test.close()
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='to do')
+	parser.add_argument('-train_data', type=str, default='../../data/Listcrawler_baseline.tsv', help='filepath of your custom train dataset as csv or tsv')
+	parser.add_argument('-test_data', type=str, default='../../data/CanadaMax80_results.csv', help='filepath of your custom train dataset as csv or tsv')
+
+	parser.add_argument('-train_dc', type=str, default='description', help='name of the description column of train data')
+	parser.add_argument('-train_tc', type=str, default='MTurk_2', help='name of the column of ner tags in required format of train data')
+
+	parser.add_argument('-test_dc', type=str, default='description', help='name of the description column of test data')
+	parser.add_argument('-test_tc', type=str, default='TRUE', help='name of the column of ner tags in required format of test data')
+
+	args = parser.parse_args()
+
+	data_train, data_test = load_custom_data(args.train_data, args.test_data, args.train_dc, args.test_dc, args.train_tc, args.test_tc)
+	#print(data_train.head())
+	exit()
+	conll = load_conll()
+	new_train, new_test = merge("/home/pnair6/McGill/Research/HT/NER/bert_ner_fine_tune/data_feb18/ht_formatted_data", conll)
+
+	# new_train.to_csv("custom_ht_train_data.csv")
+	# new_test.to_csv("custom_ht_test_data.csv")
+	#new_train = pd.read_csv("conll_ht_train_data.csv")
+	#new_test = pd.read_csv("conll_ht_test_data.csv")
+
+	csv_to_txt(new_train, new_test)
