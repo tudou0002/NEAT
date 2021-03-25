4374a20a2bfecbca7c9954b7abfe14a4d76a639e
tudou0002 <1244491123@qq.com>
2021-03-25 13:03:28 -0400

    Added the confidence score for each pattern

--- extractors/rule_extractor.py
+++ extractors/rule_extractor.py
@@ -8,15 +8,17 @@ class RuleExtractor(Extractor):
     def __init__(self, **kwargs):
         model = kwargs.pop('model', 'en_core_web_sm')
         Extractor.__init__(self, model)
-        self.patterns = self.define_patterns()
+        self.patterns,self.weights = self.define_patterns()
         self.matcher = self.create_matcher(self.patterns)
-        # self.embedding = FasttextEmbeddings()
         self.type = 'rule'
         
     def create_matcher(self,  patterns):
         matcher = Matcher(self.nlp.vocab)
-        matcher.add('thirdMatch',  self.patterns[0])
-        matcher.add('secondMatch',  self.patterns[1])
+        for k,v in self.patterns.items():
+            matcher.add(k,[v])
+#         matcher.add('thirdMatch',  self.patterns[0])
+#         matcher.add('secondMatch',  self.patterns[1])
+#         matcher.add('firstMatch', self.patterns[2])
         return matcher
     
     def define_patterns(self):
@@ -38,10 +40,23 @@ class RuleExtractor(Extractor):
         pattern14 = [{"LOWER":"mz."},{"TAG": "NNP"}]
         pattern15 = [{"LOWER":"named"},{"TAG": "NNP"}]
     
-        return ([pattern0,pattern1, pattern2, pattern3, pattern4],
-                [pattern5, pattern6, pattern7,pattern8,pattern9,pattern10,
-                pattern11,pattern12,pattern13,pattern14,pattern15])
-    
+        # return ([pattern0,pattern1, pattern2, pattern3, pattern4],
+        #         [pattern5, pattern6, pattern7,pattern8,pattern9,pattern10,
+        #         pattern11,pattern12,pattern13,pattern14,pattern15])
+        patterns={'pattern0':pattern0, 'pattern1':pattern1,'pattern2':pattern2,
+               'pattern3':pattern3,'pattern4':pattern4,'pattern5':pattern5,
+               'pattern6':pattern6,'pattern7':pattern7,'pattern8':pattern8,
+               'pattern9':pattern9,'pattern10':pattern10,'pattern11':pattern11,
+               'pattern12':pattern12,'pattern13':pattern13,'pattern14':pattern14,
+                'pattern15':pattern15}
+        weights = {'pattern0':(2, 0.2), 'pattern1':(2, 0.77),'pattern2':(2, 0.62),
+               'pattern3':(2, 0.47),'pattern4':(2, 0.11),'pattern5':(1, 0.47),
+               'pattern6':(1, 0.47),'pattern7':(1, 0.4),'pattern8':(1, 0.54),
+               'pattern9':(1, 0.47),'pattern10':(1, 0.47),'pattern11':(1, 0.47),
+               'pattern12':(1, 0.66),'pattern13':(1, 0.47),'pattern14':(1, 0.47),
+                'pattern15':(1, 0.47)}
+        return (patterns,weights)
+        
     def extract(self, text):
         if type(text)==float:
             return []
@@ -50,13 +65,16 @@ class RuleExtractor(Extractor):
         result = []
         for match_id, start, end in matches:
             string_id = self.nlp.vocab.strings[match_id]
-            if string_id=='thirdMatch':
-                name_start = start + 2
-            else:
-                name_start = start + 1
+#             if string_id=='thirdMatch':
+#                 name_start = start + 2
+#             elif string_id=='secondMatch':
+#                 name_start = start + 1
+#             else:
+#                 name_start = start
+            name_start=start+self.weights[string_id][0]
             span = doc[name_start:end] 
             ent = Entity(span.text,span.start, self.type)
             # ent.score = self.embedding.get_certainty(ent.text)
-            ent.confidence = 0.4
+            ent.confidence = self.weights[string_id][1]
             result.append(ent)
         return result
